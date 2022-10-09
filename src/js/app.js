121f2c6250608d70642537ea8150ca621f6df2af
test_2022 <prudkyi87@gmail.com>
2022-10-09 20:43:53 +0200

    Продовжумємо над цим проектом

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,29 @@ window.addEventListener('scroll', e => {
 })
 
 
+
+
+let arrowList_transport = ['BMW', 'Audio', 'Mersedes'],
+    arrowList_bikes = ['Yamaha'];
+
+
+const arr = new Map([
+    ['Car', arrowList_transport],
+    ['Bikes', arrowList_bikes]
+]);
+
+let firstList = document.getElementById("firstList");
+
+firstList.oninput = function() {
+    let valueFirstList = this.value;
+    for (let mainList of arr.keys()) {
+        if (mainList === valueFirstList)
+        {
+            let newArr = arr.get(valueFirstList);
+            for (let i = 0; i < newArr.length; i++)
+            {
+                console.log(newArr[i])
+            }
+        }
+    }
+};
